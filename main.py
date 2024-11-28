2ea03e22c7310e31c5219d75e51bbabda554f55e
tryevertthhub <tryevertth@gmail.com>
2024-11-28 10:56:55 -0800

    chore: update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from utils.solana_keygen import generate_solana_wallet
 from utils.time_utils import get_us_time
 from dashboards.main_dashboard import MainDashboard
 from dashboards.afk_dashboard import AfkDashboard
+from dashboards.position_dashboard import PositionDashboard
 
 def main():
     """Run the bot."""
@@ -11,14 +12,18 @@ def main():
     time_utils = get_us_time
     main_dashboard = MainDashboard(wallet_generator, time_utils)
     afk_dashboard = AfkDashboard(wallet_generator, time_utils, main_dashboard)
+    position_dashboard = PositionDashboard(wallet_generator, time_utils, main_dashboard)
 
     application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", main_dashboard.show))
     application.add_handler(CallbackQueryHandler(main_dashboard.show, pattern="main"))
-    
+
     application.add_handler(CallbackQueryHandler(afk_dashboard.show, pattern="afk_mode"))
     application.add_handler(CallbackQueryHandler(afk_dashboard.handle_button_click, pattern="afk_mode|back_to_main|refresh"))
+
+    application.add_handler(CallbackQueryHandler(position_dashboard.show, pattern="positions"))
+    application.add_handler(CallbackQueryHandler(position_dashboard.handle_button_click, pattern="back_to_main|refresh"))
 
     application.run_polling()
 
